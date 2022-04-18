f71c3698d00f1e51070d5f38ca5b5925d93648d3
hinesqui <hinesqui@gmail.com>
2022-04-19 02:19:46 +0300

    add db connections

--- src/db/elastic.py
+++ src/db/elastic.py
@@ -2,8 +2,10 @@ from typing import Optional
 
 from elasticsearch import AsyncElasticsearch
 
+from src.core.config import ELASTIC_HOST
+
 es: Optional[AsyncElasticsearch] = None
 
 
 async def get_elastic() -> AsyncElasticsearch:
-    return es
+    return AsyncElasticsearch(ELASTIC_HOST)
--- src/db/redis.py
+++ src/db/redis.py
@@ -1,9 +1,12 @@
 from typing import Optional
 
+import aioredis
 from aioredis import Redis
+
+from core.config import REDIS_URL
 
 redis: Optional[Redis] = None
 
 
 async def get_redis() -> Redis:
-    return redis
+     return await aioredis.from_url(REDIS_URL, max_connections=20, decode_responses=True)
