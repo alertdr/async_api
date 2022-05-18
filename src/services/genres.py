702114516bb9c850535933d269f887c1d55a4544
hinesqui <hinesqui@gmail.com>
2022-05-19 00:22:33 +0300

    add abstract classes for cache/search

--- src/services/genres.py
+++ src/services/genres.py
@@ -1,15 +1,12 @@
 import logging
 from functools import lru_cache
 
-from aioredis import Redis
-from elasticsearch import AsyncElasticsearch
 from fastapi import Depends
 
 from db.elastic import get_elastic
 from db.redis import get_redis
 from models.genre import Genre
-
-from .basic import BaseService
+from .basic import BaseService, AsyncCacheStorage, AsyncFullTextSearch
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +22,7 @@ class GenreService(BaseService):
 
 @lru_cache()
 def get_genre_service(
-        redis: Redis = Depends(get_redis),
-        elastic: AsyncElasticsearch = Depends(get_elastic),
+        cache: AsyncCacheStorage = Depends(get_redis),
+        searcher: AsyncFullTextSearch = Depends(get_elastic),
 ) -> GenreService:
-    return GenreService(redis, elastic)
+    return GenreService(cache, searcher)
