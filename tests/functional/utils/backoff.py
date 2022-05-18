1a133338f5f4ad066ea0320544f48f357ac6e060
hinesqui <hinesqui@gmail.com>
2022-05-18 21:39:18 +0300

    rm docstring

--- tests/functional/utils/backoff.py
+++ tests/functional/utils/backoff.py
@@ -4,16 +4,6 @@ from time import sleep
 
 
 def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
-    """
-    Функция для повторного выполнения функции через некоторое время, если возникла ошибка.
-    Использует наивный экспоненциальный рост времени повтора (factor) до граничного времени ожидания (border_sleep_time)
-
-    :param start_sleep_time: начальное время повтора
-    :param factor: во сколько раз нужно увеличить время ожидания
-    :param border_sleep_time: граничное время ожидания
-    :return: результат выполнения функции
-    """
-
     def func_wrapper(func):
         @wraps(func)
         def inner(*args, **kwargs):
